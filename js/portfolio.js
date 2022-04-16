a2cc34663332d1d4353820c089c0ddd46789d0ec
babaGie <rylepio79@gmail.com>
2022-04-16 11:36:06 +0800

    added new files to my portfolio

---
+++ js/portfolio.js
@@ -0,0 +1,11 @@
+var typed = new Typed('#typed', {
+    strings: [
+        "&nbsp;am a Full-Stack Web Developer.&nbsp;",
+        "&nbsp;am a Programmer.&nbsp;",
+        "&nbsp;aspire to be an IT Professor.&nbsp;",
+        "&nbsp;solve Problems.&nbsp;"
+    ],
+    typeSpeed: 53,
+    backSpeed: 50,
+    loop: true
+  });
